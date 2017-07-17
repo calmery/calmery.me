611bd649827ed5e32f0f62ff9568dac73c95027d
Marei Kikukawa <contact@calmery.me>
2017-07-17 10:26:25 +0900

    Add switch icon test

--- Elements/Icon.elm
+++ Elements/Icon.elm
@@ -1,7 +1,18 @@
-module Elements.Icon exposing ( icon )
+module Elements.Icon exposing ( Icon, icon )
 
 import Html exposing ( Html, div )
 import Html.Attributes exposing ( .. )
+
+type alias Icon =
+  { src : String
+  , line_src : String
+  }
+
+icons : List Icon
+icons = [ Icon "icon1.png" "icon_line1.jpg"
+        , Icon "icon2.jpg" "icon_line2.jpg"
+        , Icon "icon3.png" "icon_line3.jpg"
+        ]
 
 icon : Html msg
 icon =
