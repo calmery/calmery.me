dec1aeb202cf79e186ab0fb4dcd26ac5100a20a0
Marei Kikukawa <contact@calmery.me>
2018-10-21 01:42:01 +0900

    Change to embed blog data

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -3,20 +3,19 @@ module Main exposing (init, main, subscriptions)
 import Browser exposing (element)
 import Model exposing (Model)
 import Data.Qiita exposing (getQiita)
-import Data.Blog exposing (getBlog)
-import Ports exposing (setTitle, parsedBlog, parsedQiita)
+import Ports exposing (setTitle, parsedQiita)
 import Update exposing (Msg(..), update)
 import View exposing (view)
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( Model [] [], Cmd.batch [ setTitle "Calmery.me", getQiita, getBlog ] )
+    ( Model [] [], Cmd.batch [ setTitle "Calmery.me", getQiita ] )
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.batch [ parsedBlog ParsedBlog, parsedQiita ParsedQiita ]
+    Sub.batch [ parsedQiita ParsedQiita ]
 
 
 main : Program () Model Msg
