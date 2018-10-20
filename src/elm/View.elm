33ffe9d8cb6d3f8e4ef2caefa7195754ea4d9762
Marei Kikukawa <contact@marei.jp>
2018-10-21 01:42:01 +0900

    Change to embed blog data

--- src/elm/View.elm
+++ src/elm/View.elm
@@ -2,7 +2,7 @@ module View exposing (view)
 
 import Html exposing (Html, div, text)
 import Html.Attributes exposing (class)
-import Model exposing (Model)
+import Model exposing (Model, Article)
 import Update exposing (Msg)
 import Part.Article exposing (viewArticles)
 import Part.Header exposing (viewHeader)
@@ -32,7 +32,11 @@ view model =
             , viewMenus
             , div [ class "contents" ]
                 [ flex
-                    [ viewArticles "Blog" "http://calmery.hatenablog.com" model.blog
+                    [ viewArticles "Blog"
+                        "http://calmery.hatenablog.com"
+                        [ Article "メルカリのインターンシップに行ってきた" [ "Internship" ] "2018/10/06" "http://calmery.hatenablog.com/entry/2018/10/06/121419"
+                        , Article "Mint を試してみた" [ "Mint" ] "2018/08/05" "http://calmery.hatenablog.com/entry/2018/08/05/032432"
+                        ]
                     , viewArticles "Qiita" "https://qiita.com/calmery" model.qiita
                     ]
                 ]
