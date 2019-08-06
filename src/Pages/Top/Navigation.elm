0a03ac7d1e8acd9a1e927bcd09af2e8878cb3d81
Marei Kikukawa <contact@marei.jp>
2019-08-06 18:16:13 +0900

    Add navigation

---
+++ src/Pages/Top/Navigation.elm
@@ -0,0 +1,28 @@
+module Pages.Top.Navigation exposing (view)
+
+import Html exposing (Html, a, div, img, nav, span, text)
+import Html.Attributes exposing (class, href, src)
+import Msg exposing (Msg)
+
+
+view : Html Msg
+view =
+    nav
+        []
+        [ link "Twitter" "https://twitter.com/calmeryme"
+        , link "GitHub" "https://github.com/calmery"
+        , link "Qiita" "https://qiita.com/calmery"
+        , link "Blog" "http://calmery.hatenablog.com"
+        , link "Calmery.moe" "https://calmery.moe"
+        ]
+
+
+link : String -> String -> Html Msg
+link name url =
+    a [ href url ]
+        [ div
+            [ class "link" ]
+            [ div [] [ img [ src ("assets/" ++ name ++ ".svg") ] [] ]
+            , div [] [ text name ]
+            ]
+        ]
