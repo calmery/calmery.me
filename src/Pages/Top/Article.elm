dddcb99459714f2280f4d66fe01c5f2f94c4eaee
Marei Kikukawa <contact@calmery.me>
2019-08-06 21:23:26 +0900

    Change to display blog articles

--- src/Pages/Top/Article.elm
+++ src/Pages/Top/Article.elm
@@ -4,6 +4,7 @@ import Html exposing (Html, a, article, div, h2, text)
 import Html.Attributes exposing (class, href, id)
 import Model exposing (Model)
 import Msg exposing (Msg)
+import Pages.Top.Article.Blog as Blog
 import Pages.Top.Article.Qiita as Qiita
 
 
@@ -12,32 +13,5 @@ view model =
     div
         [ id "articles" ]
         [ Qiita.view model
-        ]
-
-
-articleView : String -> String -> Html Msg
-articleView title url =
-    article
-        []
-        [ h2
-            []
-            [ text title ]
-        , a [ href "" ]
-            [ div
-                [ class "article" ]
-                [ div [ class "title" ] [ text "メルカリのインターンシップに参加してきた" ]
-                , div [ class "footer" ]
-                    [ div [ class "tag" ] [ text "Internship" ]
-                    , div [ class "date" ] [ text "2019/10/10" ]
-                    ]
-                ]
-            ]
-        , div
-            [ class "article" ]
-            [ text "Hello" ]
-        , a [ href url ]
-            [ div
-                [ class "read-more" ]
-                [ text "Read More" ]
-            ]
+        , Blog.view
         ]
