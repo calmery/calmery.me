10b756495974b2029d55ed81e11d4e5de339218f
Marei Kikukawa <contact@marei.jp>
2019-08-06 17:34:58 +0900

    Update header layout

--- src/Pages/Top.elm
+++ src/Pages/Top.elm
@@ -2,12 +2,10 @@ module Pages.Top exposing (view)
 
 import Html exposing (Html, div, text)
 import Model exposing (Model)
-import Pages.Top.Articles as Articles
 import Pages.Top.Header as Header
 
 
 view model =
     div []
         [ Header.view model
-        , Articles.view model
         ]
