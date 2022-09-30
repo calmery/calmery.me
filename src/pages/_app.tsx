da76ff3c7c4b72e67799ce7a6f5121778158e911
calmery <calmery@users.noreply.github.com>
2022-10-01 06:16:49 +0900

    GA のコードを削除、index.tsx から不要なコードを削除する

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,13 +10,6 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
         <meta content="#FFFFFF" name="theme-color" />
         <meta content="initial-scale=1.0, width=device-width" name="viewport" />
         <title>Calmery.me</title>
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=UA-126557808-2"
-        />
-        <script>
-          {`window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'UA-126557808-2');`}
-        </script>
       </Head>
       <Component {...pageProps} />
     </>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -92,52 +92,43 @@ const Sidebar: React.FC = () => {
 
 const Index: React.FC = () => {
   return (
-    <>
-      <Head>
-        <title>Calmery.me</title>
-        <meta
-          content={`Calmery（@calmeryme）のプロフィールページです`}
-          name="description"
-        />
-      </Head>
-      <div className={clsx("container", "mx-auto", "select-none")}>
+    <div className={clsx("container", "mx-auto", "select-none")}>
+      <div
+        className={clsx(
+          "flex",
+          "mx-32",
+          "gap-48",
+          "mobile:flex-col",
+          "mobile:gap-32"
+        )}
+      >
         <div
-          className={clsx(
-            "flex",
-            "mx-32",
-            "gap-48",
-            "mobile:flex-col",
-            "mobile:gap-32"
-          )}
+          className={clsx("desktop:h-fit", "desktop:sticky", "desktop:top-0")}
         >
           <div
-            className={clsx("desktop:h-fit", "desktop:sticky", "desktop:top-0")}
+            className={clsx(
+              "mt-48",
+              "desktop:flex",
+              "desktop:flex-col",
+              "desktop:gap-32",
+              "desktop:mb-48",
+              "desktop:w-[19rem]"
+            )}
           >
-            <div
-              className={clsx(
-                "mt-48",
-                "desktop:flex",
-                "desktop:flex-col",
-                "desktop:gap-32",
-                "desktop:mb-48",
-                "desktop:w-[19rem]"
-              )}
-            >
-              <Sidebar />
-              <div className={clsx("mobile:hidden")}>
-                <Footer />
-              </div>
+            <Sidebar />
+            <div className={clsx("mobile:hidden")}>
+              <Footer />
             </div>
           </div>
-          <div className={clsx("my-48", "w-full", "mobile:my-0")}>
-            <Main />
-          </div>
-          <div className={clsx("desktop:hidden", "mobile:mb-48")}>
-            <Footer />
-          </div>
+        </div>
+        <div className={clsx("my-48", "w-full", "mobile:my-0")}>
+          <Main />
+        </div>
+        <div className={clsx("desktop:hidden", "mobile:mb-48")}>
+          <Footer />
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
