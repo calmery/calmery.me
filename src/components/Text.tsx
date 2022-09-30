3dd5d7dd321a412fe9ceb363f1556f8e14e126f8
calmery <calmery@users.noreply.github.com>
2022-10-01 02:09:47 +0900

    `Text` コンポーネントに `primary` などのオプションを追加する

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -2,12 +2,25 @@ import clsx from "clsx";
 import type { DetailedHTMLProps, HTMLAttributes } from "react";
 
 export const Text: React.FC<
-  DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>
-> = (props) => {
+  DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> & {
+    primary?: boolean;
+    secondary?: boolean;
+    tertiary?: boolean;
+  }
+> = ({ primary, secondary, tertiary, ...props }) => {
   return (
     <div
       {...props}
-      className={clsx("[&>*img]:rounded", "text-14", props.className)}
+      className={clsx(
+        "[&>*img]:rounded",
+        "text-14",
+        {
+          "text-primary": primary,
+          "text-secondary": secondary,
+          "text-tertiary": tertiary,
+        },
+        props.className
+      )}
     />
   );
 };
