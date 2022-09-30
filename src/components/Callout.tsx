b66089832198e0f4acac93a4b5b21491db078906
calmery <calmery@users.noreply.github.com>
2022-10-01 02:09:27 +0900

    `Callout` コンポーネントを追加する

---
+++ src/components/Callout.tsx
@@ -0,0 +1,26 @@
+import clsx from "clsx";
+import { Icon, type IconProps } from "~/components/Icon";
+import { Text } from "~/components/Text";
+
+export const Callout: React.FC<{
+  children: string;
+  icon: IconProps["icon"];
+}> = ({ children, icon }) => {
+  return (
+    <div
+      className={clsx(
+        "border",
+        "border-secondary",
+        "flex",
+        "gap-8",
+        "p-8",
+        "rounded"
+      )}
+    >
+      <div className={clsx("flex-shrink-0")}>
+        <Icon icon={icon} size={18} />
+      </div>
+      <Text secondary>{children}</Text>
+    </div>
+  );
+};
