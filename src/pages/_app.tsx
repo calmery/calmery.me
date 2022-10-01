208b674a7c03420a50d7bc83b2d0ebbaaff2075b
calmery <calmery@users.noreply.github.com>
2022-10-01 13:57:55 +0900

    `meta` タグなどを設定する

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,10 +6,44 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
       <Head>
-        <link href="/icon.webp" rel="icon" />
-        <meta content="#FFFFFF" name="theme-color" />
-        <meta content="initial-scale=1.0, width=device-width" name="viewport" />
-        <title>Calmery.me</title>
+        <link href="/favicon.ico" rel="icon" />
+
+        <meta
+          name="format-detection"
+          content="address=no, email=no, telephone=no"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:creator" content="@calmeryme" />
+        <meta
+          name="twitter:description"
+          content="Calmeryのプロフィールページです。"
+        />
+        <meta name="twitter:site" content="@calmeryme" />
+        <meta name="twitter:title" content="Calmery.me" />
+        <meta name="twitter:image" content="/icon.jpg" />
+
+        {/* Open Graph */}
+        <meta
+          property="og:description"
+          content="Calmeryのプロフィールページです。"
+        />
+        <meta property="og:image" content="/icon.jpg" />
+        <meta property="og:image:height" content="256" />
+        <meta property="og:image:width" content="256" />
+        <meta property="og:locale" content="ja_JP" />
+        <meta property="og:site_name" content="Calmery.me" />
+        <meta property="og:title" content="Calmery.me" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://calmery.me" />
+        <meta property="profile:first_name" content="Marei" />
+        <meta property="profile:last_name" content="Watanabe" />
+        <meta property="profile:gender" content="male" />
+        <meta property="profile:username" content="Calmery" />
       </Head>
       <Component {...pageProps} />
     </>
