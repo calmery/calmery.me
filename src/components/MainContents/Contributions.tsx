f9ea7d5b92f804184bfc9088c596f530798ecc09
calmery <12670155+calmery@users.noreply.github.com>
2023-10-03 11:52:10 +0900

    GitHub リポジトリを移行したことでコントリビューション一覧に不要な PR が表示されてしまうので URL を更新する

--- src/components/MainContents/Contributions.tsx
+++ src/components/MainContents/Contributions.tsx
@@ -10,7 +10,7 @@ export const Contributions: React.FC = () => {
         OSS へのコントリビューションなど
       </Heading>
       <Text>
-        <ExternalLink href="https://github.com/search?q=author%3Acalmery+is%3Apr+-repo%3Acalmery%2Fcalmery.me+-repo%3Acalmery%2FCcpvApi+-repo%3Acalmery%2FCcpvApp+-repo%3Acalmery%2FCcpvDocs+-repo%3Acalmery%2FDataListJS+-repo%3Acalmery%2Felm-boilerplate+-repo%3Acalmery%2FLegacySpotlight+-repo%3Acalmery%2FTheater+-repo%3Acalmery-chan%2Fcalmery.moe+-repo%3Acalmery-chan%2Fcreamsoda.in+-repo%3Acalmery-chan%2Fmetaneno.art+-repo%3Ajphacks%2FFK_1801+-repo%3Anaoyasugita%2Fspotlight-server+-repo%3Anichikan%2Fnichikan.github.io+-repo%3Ayuyan0617%2Fyuyan0617.github.io&type=Issues">
+        <ExternalLink href="https://github.com/search?q=author%3Acalmery+is%3Apr+-repo%3Acalmery%2Fcalmery-chan-camera+-repo%3Acalmery%2Fcalmery.me+-repo%3Acalmery%2Fcalmery.moe+-repo%3Acalmery%2FCcpvApi+-repo%3Acalmery%2FCcpvApp+-repo%3Acalmery%2FCcpvDocs+-repo%3Acalmery%2Fcreamsoda.in+-repo%3Acalmery%2FDataListJS+-repo%3Acalmery%2Felm-boilerplate+-repo%3Acalmery%2FLegacySpotlight+-repo%3Acalmery%2Fmetaneno.art+-repo%3Acalmery%2FTheater+-repo%3Acalmery-chan%2Fcalmery.moe+-repo%3Acalmery-chan%2Fcreamsoda.in+-repo%3Acalmery-chan%2Fmetaneno.art+-repo%3Ajphacks%2FFK_1801+-repo%3Anaoyasugita%2Fspotlight-server+-repo%3Anichikan%2Fnichikan.github.io+-repo%3Ayuyan0617%2Fyuyan0617.github.io&type=pullrequests">
           GitHub
         </ExternalLink>{" "}
         で確認できます。
